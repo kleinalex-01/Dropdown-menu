67c557bbc53a2fecd61ea185e7c84888f44a7cce
kleinalex-01 <152911144+kleinalex-01@users.noreply.github.com>
2024-09-18 16:33:59 +0200

    Project finished
    Made a simple toggle function and played around with a random color changing function.

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const menuContainer = document.getElementById("menu-container");
+const foodSelector = document.getElementById("food-selector");
 
-const colorMixer = (id) => {
+const colorMixer = (element) => {
     const nums = [];
     for (i = 0; i < 3; i++) {
         const randomNum = Math.floor(Math.random() *256);
         nums.push(randomNum);
     }
-    return document.getElementById(id).style.color(`rgb(${nums})`);
+    console.log(nums)
+    return element.style.color = `rgb(${nums})`;
 
 }
 
+const dropList = document.getElementById("dropdown-list");
 
-colorMixer("menu-container")
+
+const toggleList = () => {
+    if (dropList.style.visibility === "hidden") {
+        dropList.style.visibility = "visible";
+    } else {
+        dropList.style.visibility = "hidden";
+    }
+};
+
+menuContainer.addEventListener('click',toggleList);
